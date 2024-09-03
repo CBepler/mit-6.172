1baf4908d3db8dc143923d97d0574c568bd126fa
Christian Bepler <bepler135@gmail.com>
2024-09-03 13:22:54 -0400

    complete checkpoint 2

--- hw3/recitation3/example1.c
+++ hw3/recitation3/example1.c
@@ -9,6 +9,9 @@
 void test(uint8_t * restrict a,  uint8_t * restrict b) {
   uint64_t i;
 
+  a = __builtin_assume_aligned(a, 32);
+  b = __builtin_assume_aligned(b, 32);
+
   for (i = 0; i < SIZE; i++) {
     a[i] += b[i];
   }
