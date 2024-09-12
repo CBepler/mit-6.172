7a0b81db7bcdd90976c6b5eab903da849c6f12c4
Christian Bepler <bepler135@gmail.com>
2024-09-12 13:53:16 -0400

    add coarsening

--- hw4/fib.c
+++ hw4/fib.c
@@ -1,8 +1,17 @@
-int fibonacci(int n) {
-    if(n < 2) {
-        return n;
+#include <omp.h>
+
+
+
+int64_t fibonacci(int64_t n) {
+    static int64_t fib[9] = {0, 1, 1, 2, 3, 5, 8, 13, 21};
+    if(n < 9) {
+        return fib[n];
     }
-    int x = fibonacci(n - 1);
-    int y = fibonacci(n - 2);
+    int64_t x, y;
+#pragma omp task shared(x, n)
+    x = fibonacci(n - 1);
+#pragma omp task shared(y, n)
+    y = fibonacci(n - 2);
+#pragma omp taskwait
     return x + y;
 }
