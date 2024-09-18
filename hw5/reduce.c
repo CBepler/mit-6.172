e137de3d3877b640a7bf6e5181aeddf4fecbd9e3
Christian Bepler <bepler135@gmail.com>
2024-09-17 22:17:53 -0400

    complete writeup 7

--- hw5/reduce.c
+++ hw5/reduce.c
@@ -3,15 +3,26 @@
 #include <stdlib.h>
 
 int reduce(int* A, int n) { //assume n is multiple of 8;
-    int index = 0;
-    #pragma omp parallel for shared(A, index)
-    for(int i = 0; i < n; i += 8){
-        A[index] = A[i] + A[i + 1] + A[i + 2] + A[i + 3] + A[i + 4] + A[i + 5] + A[i + 6] + A[i + 7];
-        ++index;
-    }
     int sum = 0;
-    for(int i = 0; i < index; ++i) {
-        sum += A[i];
+    #pragma omp parallel shared(A)
+    {
+        int local_sum = 0;
+        #pragma omp for
+        for(int i = 0; i < n; i += 8){
+            local_sum = A[i] + A[i + 1] + A[i + 2] + A[i + 3] + A[i + 4] + A[i + 5] + A[i + 6] + A[i + 7];
+        }
+
+        #pragma omp atomic
+        sum += local_sum;
+    }
+    return sum;
+}
+
+int better_reduce(int* A, int n) {
+    int sum = 0;
+    #pragma omp parallel for reduction(+:sum)
+    for(int i = 0; i < n; ++i) {
+        sum += *(A + i);
     }
     return sum;
 }
@@ -21,8 +32,9 @@ int main(int argc, char const *argv[])
     int* A = malloc(sizeof(int) * 64);
     int n = 64;
     for(int i = 0; i < n; ++i) {
-        *(A + 1) = 1;
+        *(A + i) = 1;
     }
-    printf("%d", reduce(A, n));
+    printf("%d\n", reduce(A, n));
+    printf("%d", better_reduce(A, n));
     return 0;
 }
