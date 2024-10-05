1a2294e8e873672de574f21d75df2225dd161002
Christian Bepler <bepler135@gmail.com>
2024-10-04 21:54:00 -0400

    start binned list implementation

---
+++ project3/mymalloc/binned_free_list.c
@@ -0,0 +1,48 @@
+#include <stddef.h>
+#include <stdbool.h>
+#include "binned_free_list.h"
+#include "tools.h"
+
+binned_free_list* make_binned_list(size_t num_bins, size_t min_bin_size) {
+    binned_free_list* list = (binned_free_list*)malloc(sizeof(binned_free_list));
+    for(size_t i = 0; i < num_bins; ++i) {
+        *(list->bins + i) = make_linked_list(2 << i + min_bin_size);
+    }
+    list->min_bin_size = min_bin_size;
+    list->num_bins = num_bins;
+}
+
+void* remove(binned_free_list* list, size_t num_bytes) {
+    num_bytes = round_up_to_power_of_2(num_bytes);
+    int bin = log2_power_of_2(num_bytes) - list->min_bin_size;
+    void* address = ll_remove(*(list->bins + bin), 0);
+    if(address != NULL) return address;
+    //otherwise no items in this and bin will have to break larger block and potentially combine and get more memory
+}
+
+void add(binned_free_list* list, void* address, size_t num_bytes) {
+    int bin = log2_power_of_2(num_bytes) - list->min_bin_size;
+    ll_add_new_node(*(list->bins + bin), address);
+}
+
+bool break_larger_blocks(binned_free_list* list, size_t num_bytes) {
+
+}
+
+void combine(binned_free_list* list) {
+
+}
+
+bool get_more_memory(binned_free_list* list) {
+
+}
+
+void free_binned_list(binned_free_list* list) {
+    if(list == NULL) return;
+
+    for(size_t i = 0; i < list->num_bins; ++i) {
+        free_linked_list(*(list->bins + i));
+    }
+
+    free(list);
+}
